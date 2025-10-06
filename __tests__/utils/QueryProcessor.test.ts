b3f13466c2670facbc028bc0fb4a7073fc518ba1
ryan-twang <ryanw3@andrew.cmu.edu>
2025-10-06 14:19:43 +0000

    added query: what is your andrew id?

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -22,7 +22,15 @@ describe("QueryProcessor", () => {
         const query = "What is your name?";
         const response: string = QueryProcessor(query);
         expect(response).toBe((
-            "Rohan"
+            "Ryan Wang"
           ));
     })
+
+    test('should return Andrew ID', () => {
+        const query = "What is your Andrew ID?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "My Andrew ID is ryanw3."
+        ));
+    });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,11 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "Ryan Wang";
+  }
+
+  if (query.toLowerCase().includes("andrew id")) {
+    return "My Andrew ID is ryanw3.";
   }
 
   return "";
